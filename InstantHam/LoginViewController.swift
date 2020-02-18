a7db8fa96df98258d27d3e2181680e4c05d0d346
Emily ou <emilyou34@gmail.com>
2020-02-17 20:27:38 -0500

    Added sign up button functionality

---
+++ InstantHam/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginViewController.swift
+//  InstantHam
+//
+//  Created by Emily Ou on 2/17/20.
+//  Copyright © 2020 Emily Ou. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func signInButton(_ sender: Any) {
+    }
+    
+    // Sign Up button is triggered
+    @IBAction func signUpButton(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        // if sign up is successful else
+        user.signUpInBackground { (success, Error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(Error?.localizedDescription)")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        usernameField.delegate = self
+        passwordField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == usernameField {
+            textField.text = ""
+        }
+        else if textField == passwordField {
+            textField.text = ""
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
